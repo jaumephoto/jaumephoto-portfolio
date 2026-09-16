4692f3d50f12ea394ba369ec0da7d44378be82ca
jaumephoto <jaumem.jbs@gmail.com>
2026-09-16 20:25:36 +0200

    Add files via upload

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -7,7 +7,7 @@ export function About() {
         {/* Left Column - Image */}
         <div>
           <img
-            src="https://images.unsplash.com/photo-1696273338595-178a113ead5c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwaG90b2dyYXBoZXIlMjBwcm9mZXNzaW9uYWwlMjBjYW1lcmF8ZW58MXx8fHwxNzczNTc0MDE1fDA&ixlib=rb-4.1.0&q=80&w=1080"
+            src="/jaumephoto-portfolio/About/self_portrait_about.jpg.jpg"
             alt="Photographer"
             className="w-full h-auto"
           />
