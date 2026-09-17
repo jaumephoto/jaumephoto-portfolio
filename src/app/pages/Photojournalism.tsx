5046916729f4eac196c94e415b130019ca0a2ddd
jaumephoto <jaumem.jbs@gmail.com>
2026-09-18 01:41:27 +0200

    Add files via upload

---
+++ src/app/pages/Photojournalism.tsx
@@ -0,0 +1,14 @@
+import { GalleryGrid } from "../components/GalleryGrid";
+
+const images = [];
+
+export function Photojournalism() {
+  return (
+    <div>
+      <div className="px-6 py-8 border-b border-gray-200">
+        <h1 className="text-3xl">Photojournalism</h1>
+      </div>
+      <GalleryGrid images={images} />
+    </div>
+  );
+}
