c4d436cdd18b80b83f64994b69305049e8076744
jaumephoto <jaumem.jbs@gmail.com>
2026-09-18 01:40:11 +0200

    Add files via upload

--- src/app/pages/WeddingsEvents.tsx
+++ src/app/pages/WeddingsEvents.tsx
@@ -6,7 +6,7 @@ export function WeddingsEvents() {
   return (
     <div>
       <div className="px-6 py-8 border-b border-gray-200">
-        <h1 className="text-3xl">Weddings & Events</h1>
+        <h1 className="text-3xl">Events</h1>
       </div>
       <GalleryGrid images={images} />
     </div>
